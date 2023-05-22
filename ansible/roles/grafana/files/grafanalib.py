b673cd1bc6487ed0bf9c22501053998b497a6801
AndrewB <avborovets@gmail.com>
2023-05-22 19:24:25 +0300

    grafana lib default panels are practucally done

--- ansible/roles/grafana/files/grafanalib.py
+++ ansible/roles/grafana/files/grafanalib.py
@@ -5,7 +5,7 @@ import requests
 from os import getenv
 
 from grafanalib.core import (
-    Dashboard, TimeSeries, GaugePanel,
+    Dashboard, TimeSeries, GaugePanel, Stat,
     Target, GridPos,
     OPS_FORMAT
 )
@@ -25,7 +25,8 @@ def get_dashboard_json(dashboard, overwrite=False, message="Updated by grafanlib
 
 
 def upload_to_grafana(dashboard_json, url, verify=True):
-    response = requests.post(url, auth=auth, data=dashboard_json, headers=headers)
+    response = requests.post(
+        url, auth=auth, data=dashboard_json, headers=headers)
     print(f"{response.status_code} - {response.content}")
 
 
@@ -38,33 +39,11 @@ my_dashboard = dashboard = Dashboard(
     timezone="browser",
     panels=[
         TimeSeries(
-            title="Random Walk",
-            dataSource='default',
-            targets=[
-                Target(
-                    datasource='grafana',
-                    expr='example',
-                ),
-            ],
-            gridPos=GridPos(h=8, w=16, x=0, y=0),
-        ),
-        GaugePanel(
-            title="Random Walk",
-            dataSource='default',
-            targets=[
-                Target(
-                    datasource='grafana',
-                    expr='example',
-                ),
-            ],
-            gridPos=GridPos(h=4, w=4, x=17, y=0),
-        ),
-        TimeSeries(
-            title="Prometheus http requests",
+            title="Number of requests per minute",
             dataSource='Prometheus',
             targets=[
                 Target(
-                    expr='rate(envoy_cluster_external_upstream_rq[5m])',
+                    expr='rate(envoy_cluster_upstream_rq_total[1m])',
                     legendFormat="{{ handler }}",
                     refId='A',
                 ),
@@ -72,7 +51,33 @@ my_dashboard = dashboard = Dashboard(
             unit=OPS_FORMAT,
             gridPos=GridPos(h=8, w=16, x=0, y=10),
         ),
-    ],
+        TimeSeries(
+            title="Number of all requests",
+            dataSource='Prometheus',
+            targets=[
+                Target(
+                    expr='envoy_cluster_upstream_rq_total',
+                    legendFormat="{{ handler }}",
+                    refId='A',
+                ),
+            ],
+            gridPos=GridPos(h=8, w=16, x=0, y=10),
+        ),
+        TimeSeries(
+            title="Number XXX respose code",
+            dataSource='Prometheus',
+            # graphMode='none',
+            targets=[
+                Target(
+                    expr='envoy_cluster_external_upstream_rq',
+                    legendFormat="{{ handler }}",
+                    refId='A',
+                ),
+            ],
+            gridPos=GridPos(h=8, w=16, x=0, y=10),
+            scaleDistributionType="log"
+        )
+    ]
 )
 my_dashboard_json = get_dashboard_json(my_dashboard, overwrite=True)
 upload_to_grafana(my_dashboard_json, url)
